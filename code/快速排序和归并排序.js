e0969bfc6d5493ce3c28f6a079641d81f55af7cf
chenbin.li <1015761882@qq.com>
2024-06-15 18:00:07 +0800

    feat: add new module

---
+++ code/快速排序和归并排序.js
@@ -0,0 +1,57 @@
+// 快速排序
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+console.log(quickSort([7, 5, 3, 8, 2, 4, 2, 7, 1]));
+
+// 归并排序
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  const merge = (left, right) => {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+        result.push(left[leftIndex]);
+        leftIndex++;
+      } else {
+        result.push(right[rightIndex]);
+        rightIndex++;
+      }
+    }
+
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+  };
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+const arr = [5, 3, 8, 4, 2, 7, 1];
+const sortedArr = mergeSort(arr);
+console.log(sortedArr);
